681c7a4f7f844bf3a6cceb665fd4fd8455949151
James Futhey <futhey@gmail.com>
2017-04-10 18:44:46 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,138 @@
-# electric-love
+# Electric Love
 An analytics abstraction layer. Write your events once, then send them where ever you want.
+
+## What is it?
+
+**“Electric Love”** is an extensible abstraction layer for working with common third-party Analytics libraries.
+
+Since more or less all analytics libraries work the same way (allowing you to identify and describe users, and track the events which they perform), 
+**“Electric Love”** creates an abstraction layer and a set of adapters that allow you to write generic Analytics Tracking code once, and update your
+configuration later to send your data anywhere you need.
+
+
+## How does it work?
+
+1. Install your third-party analytics libraries in your application or website (you don't need to do anything special)
+2. Install `electric-love.js` in your website or app.
+3. Instead of filling your app or website with service-specific tracking code, write generic code using the **“Electric Love” Javascript API** (described below).
+4. **“Electric Love”** will detect the third-party Analytics services you have installed on your website or app, and use it's own, community-maintained adapters to propagate your events, in an identical format, to each third-party service, using the latest version(s) of their APIs.
+5. **(Optional):** You can extend **“Electric Love”** by writing your own custom adapters for third-party services not yet supported. Each adapter only requires about 12 lines of Javascript, and the community is available to help you ship your first PR to **“Electric Love”**.
+
+
+## ElectricLove.identify(userId, userProperties)
+
+This method allows you to identify the current visitor, and (optionally) describe the user.
+
+_Only identify the user, do not describe any user properties:_
+
+```
+ElectricLove.identify('<unique-user-id>');
+```
+
+
+_Identify the user and describe user properties:_
+
+```
+ElectricLove.identify('<unique-user-id>', {
+    name: 'John Doe',
+    subscribedToNewsletter: false
+});
+```
+
+This is similar to the `Identify` method found in Mixpanel, Heap, and Analytics.js.
+
+
+## ElectricLove.track(eventName, eventProperties)
+
+This method allows you to identify the current visitor, and (optionally) describe the user.
+
+_Track a simple event, with no event properties:_
+
+```
+ElectricLove.track('click_signup_button');
+```
+
+_Track a more complex event, with event properties:_
+
+```
+ElectricLove.track('purchase', {
+    amount: 32.00,
+    itemCount: 4,
+    shippingSpeed: 'next-day'
+});
+```
+
+This is similar to the `Track` method found in Mixpanel, Heap, and Analytics.js.
+
+
+## Writing an Adapter
+
+Below is an example of a blank adapter.
+
+```
+'blank-adapter-template': { // Do not modify this template
+            enabled: false, // Change to true once you're completed testing
+            test: function () {},
+            onIdentify: function (userId, userProperties) {},
+            onTrack: function (eventName, eventProperties) {}
+        }
+```
+
+It has three main components:
+
+**1. Test:** This function, once evaluated, should provide an answer to the question “has a specific third-party analytics library been installed on this page? Is it active? Should we try to send events to this service?” 
+
+This can be as simple as sniffing the window object for a global variables, and a commonly-used (and not likely to disappear) method or property:
+
+```
+test: function () {
+    return window.ga && window.ga.loaded;
+}
+```
+
+(A simple example taken from the Google Analytics adapter)
+
+**2. onIdentify:** This function takes data from our generic `identify` method, and passes it along to a third-party library, via an adapter.
+
+This should contain a minimal number of integrity checks and transforms, as well as a lightweight wrapper for the library's identify and/or describe functionality.
+
+```
+onIdentify: function (userId, userProperties) {
+    // Send the identify call to Mixpanel's JS library
+    if (window.mixpanel && userId) 
+        mixpanel.identify(userId);
+
+    // Set people properties on our identified user
+    if (window.mixpanel && userProperties) 
+        mixpanel.people.set(userProperties);
+}
+```
+
+(A simple example taken from the Mixpanel adapter)
+
+**3. onTrack:** This function takes data from our generic `track` method, and passes it along to a third-party library, via an adapter.
+
+This should contain a minimal number of integrity checks and transforms, as well as a lightweight wrapper for the library's event tracking functionality.
+
+```
+onTrack: function (eventName, eventProperties) {
+    // Send the tracked event to Heap's JS library
+    if (window.heap && eventName) 
+    heap.track(eventName, eventProperties);
+}
+```
+
+(A simple example taken from the Heap Analytics adapter)
+
+
+## Pull Requests
+
+Todo
+
+
+## Issues
+
+Todo
+
+
+### Thanks!
---
+++ electric-love.js
@@ -0,0 +1,165 @@
+(function () {
+    /**
+     * Third-party adapters for Gumshoe.track()
+     */
+    var thirdPartyAdapters = {
+        'Segment': {
+            enabled: true,
+            test: function () {
+                return window.analytics && window.analytics.Integrations && typeof(window.analytics.Integrations) === 'object';
+            },
+            onIdentify: function (userId, userProperties) {
+                // Send the identify call to Segment's Analytics.js library
+                // console.log('Identifying: ', userId, userProperties);
+                if (window.analytics && userId) analytics.identify(userId, userProperties);
+            },
+            onTrack: function (eventName, eventProperties) {
+                // Send the tracked event to Segment's Analytics.js library
+                // console.log('tracking: ', eventName, eventProperties);
+                if (window.analytics && eventName) analytics.track(eventName, eventProperties);
+            }
+        },
+        'Mixpanel': {
+            enabled: true,
+            test: function () {
+                return window.mixpanel && window.mixpanel.__loaded;
+            },
+            onIdentify: function (userId, userProperties) {
+                // Send the identify call to Mixpanel's JS library
+                // console.log('Identifying: ', userId, userProperties);
+                if (window.mixpanel && userId) mixpanel.identify(userId);
+
+                // Set people properties on our identified user
+                if (window.mixpanel && userProperties) mixpanel.people.set(userProperties);
+            },
+            onTrack: function (eventName, eventProperties) {
+                // Send the tracked event to Mixpanel's JS library
+                // console.log('tracking: ', eventName, eventProperties);
+                if (window.mixpanel && eventName) mixpanel.track(eventName, eventProperties);
+            }
+        },
+        'Heap': {
+            enabled: true,
+            test: function () {
+                return (window.heap && window.heap.track) ? true : false;
+            },
+            onIdentify: function (userId, userProperties) {
+                // Send the identify call to Heap's JS library
+                // console.log('Identifying: ', userId, userProperties);
+                if (window.heap && userId) heap.identify(userId);
+
+                // Set people properties on our identified user
+                if (window.heap && userProperties) heap.addUserProperties(userProperties);
+            },
+            onTrack: function (eventName, eventProperties) {
+                // Send the tracked event to Heap's JS library
+                // console.log('tracking: ', eventName, eventProperties);
+                if (window.heap && eventName) heap.track(eventName, eventProperties);
+            }
+        },
+        'Intercom': {
+            enabled: true,
+            test: function () {
+                return (window.Intercom && window.Intercom('getVisitorId')) ? true : false;
+            },
+            onIdentify: function (userId, userProperties) {
+                // Send the identify call to Intercom's JS library
+                // console.log('Identifying: ', userId, userProperties);
+                if (window.Intercom && userId)
+                    Intercom('update', { user_id: userId });
+
+                // Set people properties on our identified user
+                if (window.Intercom && userProperties)
+                    Intercom('update', userProperties);
+            },
+            onTrack: function (eventName, eventProperties) {
+                // Send the tracked event to Intercom's JS library
+                // console.log('tracking: ', eventName, eventProperties);
+                if (window.Intercom && eventName)
+                    Intercom('trackEvent', eventName, eventProperties);
+            }
+        },
+        'Amplitude': {
+            enabled: true,
+            test: function () {
+                return (window.amplitude && window.amplitude.options) ? true : false;
+            },
+            onIdentify: function (userId, userProperties) {
+                // Send the identify call to Amplitude's JS library
+                // console.log('Identifying: ', userId, userProperties);
+                if (window.amplitude && userId) amplitude.getInstance().setUserId(userId);
+
+                // Set people properties on our identified user
+                if (window.amplitude && userProperties) amplitude.getInstance().setUserProperties(userProperties);
+            },
+            onTrack: function (eventName, eventProperties) {
+                // Send the tracked event to Amplitude's JS library
+                // console.log('tracking: ', eventName, eventProperties);
+                if (window.amplitude && eventName) amplitude.getInstance().logEvent(eventName, eventProperties);
+            }
+        },
+        'GoogleAnalytics': {
+            enabled: true,
+            test: function () {
+                return window.ga && window.ga.loaded;
+            },
+            onIdentify: function (userId, userProperties) {
+                // Do nothing, because, Google Analytics :(
+            },
+            onTrack: function (eventName, eventProperties) {
+                if (window.ga) ga('send', {
+                    hitType: 'event',
+                    eventCategory: 'All',
+                    eventAction: eventName
+                });
+            }
+        },
+        'blank-adapter-template': { // Do not modify this template
+            enabled: false, // Change to true once you're completed testing
+            test: function () {},
+            onIdentify: function (userId, userProperties) {},
+            onTrack: function (eventName, eventProperties) {}
+        }
+    };
+
+    function track (eventName, eventProperties) {
+        if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        for (var adapterName in thirdPartyAdapters) {
+            var adapter = thirdPartyAdapters[adapterName];
+
+            // If this adapter passes it's own internal test (usually to detect if a specific source is available)
+            if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
+                // If everything checks out for the data we've received,
+                // pass the data to the adapter so it can be tracked
+                if (adapter.onTrack && typeof(adapter.onTrack) === 'function') {
+                    adapter.onTrack(eventName, eventProperties);
+                }
+
+            }
+        }
+    }
+
+    function identify (userId, userProperties) {
+        if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        for (var adapterName in thirdPartyAdapters) {
+            var adapter = thirdPartyAdapters[adapterName];
+
+            // If this adapter passes it's own internal test (usually to detect if a specific source is available)
+            if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
+                // If everything checks out for the data we've received,
+                // pass the data to the adapter so it can be tracked
+                if (adapter.onIdentify && typeof(adapter.onIdentify) === 'function')
+                    adapter.onIdentify(userId, userProperties);
+            }
+        }
+    }
+
+    // Create / export globals
+    window.ElectricLove = {};
+    ElectricLove.thirdPartyAdapters = thirdPartyAdapters;
+    ElectricLove.identify = identify;
+    ElectricLove.track = track;
+
+})();
