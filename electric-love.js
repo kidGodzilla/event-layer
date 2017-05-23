a2c5d1a6ac563f35069c32bb68f9624d0530d221
James Futhey <futhey@gmail.com>
2017-05-23 18:29:34 +0200

    Cleanup/refactor, Flush out the script

--- electric-love.js
+++ electric-love.js
@@ -1,23 +1,39 @@
 var ElectricLove = (function ElectricLove () {
     /**
-     * Third-party adapters for Gumshoe.track()
+     * Third-party adapters for ElectricLove.track(), ElectricLove.identify(), etc.
      */
     var thirdPartyAdapters = {
+        // Todo: We should handle the case where the user aliases ElectricLove -> window.analytics
         'segment': {
             enabled: true,
             test: function () {
                 return window.analytics && window.analytics.Integrations && typeof(window.analytics.Integrations) === 'object';
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 // Send the identify call to Segment's Analytics.js library
                 // console.log('Identifying: ', userId, userProperties);
                 if (window.analytics && userId) analytics.identify(userId, userProperties);
 
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 // Send the tracked event to Segment's Analytics.js library
                 // console.log('tracking: ', eventName, eventProperties);
                 if (window.analytics && eventName) analytics.track(eventName, eventProperties);
+            },
+            page: function (category, name, properties) {
+                // Send the page call to Segment's Analytics.js library
+                // console.log('page: ', category, name, properties);
+                if (window.analytics && name) analytics.page(category, name, properties);
+            },
+            alias: function (userId, previousId) {
+                // Send the alias call to Segment's Analytics.js library
+                // console.log('alias: ', userId, previousId);
+                if (window.analytics && userId && previousId) analytics.alias(userId, previousId);
+            },
+            group: function (groupId, traits) {
+                // Send the group call to Segment's Analytics.js library
+                // console.log('group: ', groupId, traits);
+                if (window.analytics && groupId) analytics.group(groupId, traits);
             }
         },
         'mixpanel': {
@@ -25,7 +41,7 @@ var ElectricLove = (function ElectricLove () {
             test: function () {
                 return window.mixpanel && window.mixpanel.__loaded;
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 // Send the identify call to Mixpanel's JS library
                 // console.log('Identifying: ', userId, userProperties);
                 if (window.mixpanel && userId) mixpanel.identify(userId);
@@ -33,7 +49,7 @@ var ElectricLove = (function ElectricLove () {
                 // Set people properties on our identified user
                 if (window.mixpanel && userProperties) mixpanel.people.set(userProperties);
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 // Send the tracked event to Mixpanel's JS library
                 // console.log('tracking: ', eventName, eventProperties);
                 if (window.mixpanel && eventName) mixpanel.track(eventName, eventProperties);
@@ -44,7 +60,7 @@ var ElectricLove = (function ElectricLove () {
             test: function () {
                 return (window.heap && window.heap.track) ? true : false;
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 // Send the identify call to Heap's JS library
                 // console.log('Identifying: ', userId, userProperties);
                 if (window.heap && userId) heap.identify(userId);
@@ -52,7 +68,7 @@ var ElectricLove = (function ElectricLove () {
                 // Set people properties on our identified user
                 if (window.heap && userProperties) heap.addUserProperties(userProperties);
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 // Send the tracked event to Heap's JS library
                 // console.log('tracking: ', eventName, eventProperties);
                 if (window.heap && eventName) heap.track(eventName, eventProperties);
@@ -63,7 +79,7 @@ var ElectricLove = (function ElectricLove () {
             test: function () {
                 return (window.Intercom && window.Intercom('getVisitorId')) ? true : false;
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 // Send the identify call to Intercom's JS library
                 // console.log('Identifying: ', userId, userProperties);
                 if (window.Intercom && userId)
@@ -73,7 +89,7 @@ var ElectricLove = (function ElectricLove () {
                 if (window.Intercom && userProperties)
                     Intercom('update', userProperties);
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 // Send the tracked event to Intercom's JS library
                 // console.log('tracking: ', eventName, eventProperties);
                 if (window.Intercom && eventName)
@@ -85,7 +101,7 @@ var ElectricLove = (function ElectricLove () {
             test: function () {
                 return (window.amplitude && window.amplitude.options) ? true : false;
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 // Send the identify call to Amplitude's JS library
                 // console.log('Identifying: ', userId, userProperties);
                 if (window.amplitude && userId) amplitude.getInstance().setUserId(userId);
@@ -93,7 +109,7 @@ var ElectricLove = (function ElectricLove () {
                 // Set people properties on our identified user
                 if (window.amplitude && userProperties) amplitude.getInstance().setUserProperties(userProperties);
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 // Send the tracked event to Amplitude's JS library
                 // console.log('tracking: ', eventName, eventProperties);
                 if (window.amplitude && eventName) amplitude.getInstance().logEvent(eventName, eventProperties);
@@ -104,10 +120,10 @@ var ElectricLove = (function ElectricLove () {
             test: function () {
                 return window.ga && window.ga.loaded;
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 if (window.ga) ga('set', 'userId', userId);
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 if (window.ga) ga('send', {
                     hitType: 'event',
                     eventCategory: 'All',
@@ -115,32 +131,38 @@ var ElectricLove = (function ElectricLove () {
                 });
             }
         },
-        'keen': { // Do not modify this template
+        'keen': {
             enabled: true,
+            // Todo: This test sucks (keen is not opinionated as to what global you place it in, but defaults to client, which is too common to use as a test.)
             test: function () {
                 return window.Keen && window.Keen.loaded && window.client;
             },
-            onIdentify: function (userId, userProperties) {
+            identify: function (userId, userProperties) {
                 if (window.client && userId) client.extendEvents({
                     'user_id': userId
                 });
 
                 if (window.client && userProperties) client.extendEvents(userProperties);
             },
-            onTrack: function (eventName, eventProperties) {
+            track: function (eventName, eventProperties) {
                 if (window.client && eventName) client.recordEvent(eventName, eventProperties);
             }
         },
         'blank-adapter-template': { // Do not modify this template
             enabled: false,
             test: function () {},
-            onIdentify: function (userId, userProperties) {},
-            onTrack: function (eventName, eventProperties) {}
+            track: function (eventName, eventProperties) {},
+            identify: function (userId, userProperties) {},
+            page: function (category, name, properties) {},
+            alias: function (userId, previousId) {},
+            group: function (groupId, traits) {}
         }
     };
 
-    function track (eventName, eventProperties) {
+    function track (eventName, eventProperties, options, callback) {
         if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        onReady();
 
         for (var adapterName in thirdPartyAdapters) {
             var adapter = thirdPartyAdapters[adapterName];
@@ -149,16 +171,18 @@ var ElectricLove = (function ElectricLove () {
             if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
                 // If everything checks out for the data we've received,
                 // pass the data to the adapter so it can be tracked
-                if (adapter.onTrack && typeof(adapter.onTrack) === 'function') {
-                    adapter.onTrack(eventName, eventProperties);
-                }
-
+                if (adapter.track && typeof(adapter.track) === 'function')
+                    adapter.track(eventName, eventProperties);
             }
         }
+
+        if (callback && typeof(callback) === 'function') callback();
     }
 
-    function identify (userId, userProperties) {
+    function identify (userId, userProperties, options, callback) {
         if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        onReady();
 
         for (var adapterName in thirdPartyAdapters) {
             var adapter = thirdPartyAdapters[adapterName];
@@ -167,17 +191,131 @@ var ElectricLove = (function ElectricLove () {
             if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
                 // If everything checks out for the data we've received,
                 // pass the data to the adapter so it can be tracked
-                if (adapter.onIdentify && typeof(adapter.onIdentify) === 'function')
-                    adapter.onIdentify(userId, userProperties);
+                if (adapter.identify && typeof(adapter.identify) === 'function')
+                    adapter.identify(userId, userProperties);
             }
         }
+
+        if (callback && typeof(callback) === 'function') callback();
     }
+
+    function page (category, name, properties, options, callback) {
+        if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        onReady();
+
+        // Handle not passing the category (shift right)
+        if (category && (!name || typeof(name) !== 'string')) {
+            callback = options;
+            options = properties;
+            properties = name;
+            name = category;
+            category = null;
+        }
+
+        // url (canonical?), title, referrer, path
+        var url = document.querySelector("link[rel='canonical']") ? document.querySelector("link[rel='canonical']").href : document.location.href;
+        var title = document.title;
+        var referrer = document.referrer;
+        var path = location.pathname;
+
+        var props = {
+            url: url,
+            title: title,
+            referrer: referrer,
+            path: path
+        };
+
+        var properties = Object.assign(props, properties);
+
+        for (var adapterName in thirdPartyAdapters) {
+            var adapter = thirdPartyAdapters[adapterName];
+
+            // If this adapter passes it's own internal test (usually to detect if a specific source is available)
+            if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
+                // If everything checks out for the data we've received,
+                // pass the data to the adapter so it can be tracked
+                if (adapter.page && typeof(adapter.page) === 'function')
+                    adapter.page(category, name, properties);
+            }
+        }
+
+        if (callback && typeof(callback) === 'function') callback();
+    }
+
+    function group (groupId, traits, options, callback) {
+        if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        onReady();
+
+        for (var adapterName in thirdPartyAdapters) {
+            var adapter = thirdPartyAdapters[adapterName];
+
+            // If this adapter passes it's own internal test (usually to detect if a specific source is available)
+            if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
+                // If everything checks out for the data we've received,
+                // pass the data to the adapter so we can perform a grouping
+                if (adapter.group && typeof(adapter.group) === 'function')
+                    adapter.group(groupId, traits);
+            }
+        }
+
+        if (callback && typeof(callback) === 'function') callback();
+    }
+
+    function alias (userId, previousId, options, callback) {
+        if (!thirdPartyAdapters) return; // Early return if there are no adapters
+
+        onReady();
+
+        for (var adapterName in thirdPartyAdapters) {
+            var adapter = thirdPartyAdapters[adapterName];
+
+            // If this adapter passes it's own internal test (usually to detect if a specific source is available)
+            if (adapter.enabled && adapter.test && typeof(adapter.test) === 'function' && adapter.test()) {
+                // If everything checks out for the data we've received,
+                // pass the data to the adapter so we can alias this user
+                if (adapter.alias && typeof(adapter.alias) === 'function')
+                    adapter.alias(userId, previousId);
+            }
+        }
+
+        if (callback && typeof(callback) === 'function') callback();
+    }
+
+    function ready (callback) {
+        if (callback && typeof(callback) === 'function')
+            ElectricLove.readyFunction = callback;
+    }
+
+    function onReady () {
+        if (!ElectricLove.readyFunction) return;
+
+        if (ElectricLove.readyFunction && typeof(ElectricLove.readyFunction) === 'function') ElectricLove.readyFunction();
+
+        ElectricLove.readyFunction = null;
+    }
+
+    // Execute directly before the first track/identify/page/group/alias call, or after a default timeout (5s)
+    setTimeout(onReady, 5000);
+
+
+    // Todo:
+    // QueryString API?
+    // Selecting Integrations should match analytics.js syntax?
+
 
     // Create / export globals
     window.ElectricLove = {};
     ElectricLove.thirdPartyAdapters = thirdPartyAdapters;
+    ElectricLove.readyFunction = null;
     ElectricLove.identify = identify;
+    ElectricLove.onReady = onReady;
     ElectricLove.track = track;
+    ElectricLove.group = group;
+    ElectricLove.alias = alias;
+    ElectricLove.ready = ready;
+    ElectricLove.page = page;
 
     return function () {
         return ElectricLove;
