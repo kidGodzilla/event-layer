26bdb8d8c6657b8ca89041ada14960f422d71e49
James Futhey <futhey@gmail.com>
2017-05-24 19:34:32 +0300

    Update amplitude

--- electric-love.js
+++ electric-love.js
@@ -113,6 +113,19 @@ var ElectricLove = (function ElectricLove () {
                 // Send the tracked event to Amplitude's JS library
                 // console.log('tracking: ', eventName, eventProperties);
                 if (window.amplitude && eventName) amplitude.getInstance().logEvent(eventName, eventProperties);
+            },
+            page: function (category, name, properties) {
+                if (window.amplitude) {
+                    if (category || name) {
+                        if (category)
+                            amplitude.getInstance().logEvent('pageview_' + category, properties);
+
+                        if (name)
+                            amplitude.getInstance().logEvent('pageview_' + name, properties);
+                    } else {
+                        amplitude.getInstance().logEvent('pageview', properties);
+                    }
+                }
             }
         },
         'google-analytics': {
